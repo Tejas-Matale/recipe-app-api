8ee525cf3282c444f46760e122eee157baf06361
Tejas-Matale <tejasmatale99@gmail.com>
2025-06-24 01:18:12 +0530

    added sql

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+calculator function
+"""
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x+y
+
+def sub(x,y):
+    return x-y
---
+++ app/app/test.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+    def test_sub_number(self):
+        res = calc.sub(10,15)
+
+        self.assertEqual(res,-5)
